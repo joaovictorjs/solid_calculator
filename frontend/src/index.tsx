7b4076694f49bb641679236c3b4816b8d4c4e23d
joaovictorjs <skai7ami@gmail.com>
2023-02-28 09:43:30 -0300

    resize de janela

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,17 @@
 import "./styles.css";
 import { render } from "solid-js/web";
 import App from "./app/App";
+import { WindowSetSize } from "../wailsjs/runtime/runtime";
+
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    const [tgHeight, tgWidth] = [470, 260];
+    const [acHeight, acWidth] = [window.outerHeight, window.outerWidth];
+    const [diffHeight, diffWidth] = [tgHeight - acHeight, tgWidth - acWidth];
+    WindowSetSize(acWidth + diffWidth, acHeight + diffHeight);
+  },
+  { once: true }
+);
 
 render(App, document.querySelector(".root"));

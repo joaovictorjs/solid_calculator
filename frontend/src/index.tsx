9743ff09856a02b879ff4ce373a2a95b1da0e1a2
joaovictorjs <skai7ami@gmail.com>
2023-02-28 15:11:54 -0300

    resize de janela incrementado

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,34 @@
 import "./styles.css";
 import { render } from "solid-js/web";
 import App from "./app/App";
-import { WindowSetMaxSize, WindowSetMinSize } from "../wailsjs/runtime/runtime";
+import {
+  WindowGetSize,
+  WindowSetMaxSize,
+  WindowSetMinSize,
+} from "../wailsjs/runtime/runtime";
 
 document.addEventListener(
   "DOMContentLoaded",
-  () => {
-    const [targetH, targetW] = [470, 260];
-    const [height, width] = [window.outerHeight, window.outerWidth];
-    const [diffH, diffW] = [targetH - height, targetW - width];
-    WindowSetMaxSize(targetW + diffW, targetH + diffH);
-    WindowSetMinSize(targetW + diffW, targetH + diffH);
+  async () => {
+    const [targetWidth, targetHeight] = [275, 490];
+    const [currentWidth, currentHeight] = [
+      window.outerWidth,
+      window.outerHeight,
+    ];
+    const [remWidth, remHeigth] = [
+      targetWidth - currentWidth,
+      targetHeight - currentHeight,
+    ];
+
+    const actualSize = await WindowGetSize();
+
+    if (actualSize.w !== targetWidth && actualSize.h !== targetHeight) {
+      await WindowSetMaxSize(actualSize.w + remWidth, actualSize.h + remHeigth);
+      await WindowSetMinSize(actualSize.w + remWidth, actualSize.h + remHeigth);
+    } else {
+      await WindowSetMaxSize(targetWidth + remWidth, targetHeight + remHeigth);
+      await WindowSetMinSize(targetWidth + remWidth, targetHeight + remHeigth);
+    }
   },
   { once: true }
 );

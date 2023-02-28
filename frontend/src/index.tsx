ae971b44190576cf3243bd2738997e093f0da1b8
joaovictorjs <skai7ami@gmail.com>
2023-02-28 13:15:56 -0300

    fix no resize da janela

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,17 +1,18 @@
 import "./styles.css";
 import { render } from "solid-js/web";
 import App from "./app/App";
-import { WindowSetSize } from "../wailsjs/runtime/runtime";
+import { WindowSetMaxSize, WindowSetMinSize } from "../wailsjs/runtime/runtime";
 
 document.addEventListener(
   "DOMContentLoaded",
   () => {
-    const [tgHeight, tgWidth] = [470, 260];
-    const [acHeight, acWidth] = [window.outerHeight, window.outerWidth];
-    const [diffHeight, diffWidth] = [tgHeight - acHeight, tgWidth - acWidth];
-    WindowSetSize(acWidth + diffWidth, acHeight + diffHeight);
+    const [targetH, targetW] = [470, 260];
+    const [height, width] = [window.outerHeight, window.outerWidth];
+    const [diffH, diffW] = [targetH - height, targetW - width];
+    WindowSetMaxSize(targetW + diffW, targetH + diffH);
+    WindowSetMinSize(targetW + diffW, targetH + diffH);
   },
   { once: true }
 );
 
-render(App, document.querySelector(".root"));
+render(App, document.querySelector(".root") as Element);

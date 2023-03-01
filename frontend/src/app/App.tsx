37a82212dd0d4b77cff9db1952ee133e134cd0e0
joaovictorjs <skai7ami@gmail.com>
2023-03-01 14:14:13 -0300

    app

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,7 +1,34 @@
-import Keyboard from '../components/keyboard';
-import Display from '../components/display/index';
+import Keyboard from "../components/keyboard";
+import Display from "../components/display/index";
+import { deleteLast } from "../components/quick-bar";
+import { equals, store } from "../stores/calculator_store";
+import { CopyToClipboard } from "../../wailsjs/go/main/App";
 
 export default function App() {
+  document.addEventListener("keydown", async (event) => {
+    if (event.ctrlKey && event.key === "c") {
+      await CopyToClipboard(store.result.replace("= ", ""));
+      return;
+    }
+
+    if (event.key.includes("Enter")) {
+      equals();
+      return;
+    }
+
+    if (event.key.includes("Backspace")) {
+      deleteLast();
+      return;
+    }
+
+    store.keys.forEach((key) => {
+      if (key.shortcut === event.key) {
+        key.click();
+        return;
+      }
+    });
+  });
+
   return (
     <>
       <Display />
